9fe8968fbc47eba359db57950b91af97a078df76
Senkevich00 <zhenya-senkevich@mail.ru>
2020-11-23 23:34:03 +0300

    added homevork

--- main.js
+++ main.js
@@ -1,78 +1,13 @@
-const goods = [
-	{
-		name: "car 1",
-		price: 100
-	},
-	{
-		name: "car 2",
-		price: 200
-	},
-	{
-		name: "car 3",
-		price: 300
-	}
-]
+const username = localStorage.getItem('username');
 
-const goodsInStorage = JSON.parse(localStorage.getItem("goods"))
-const goodsInCart = goodsInStorage ? goodsInStorage : [];
-
-const store = document.querySelector(".root")
-const cart = document.querySelector(".cart")
-
-const addItemFromStorage = () => {
-	goodsInCart.forEach((item) => {
-		const goodInCart = document.createElement("div")
-		goodInCart.innerText = `Марка ${item.name}, цена ${item.price}`
-		cart.append(goodInCart)
-
-	})
+if (username === null || username.length === 0) {
+    document.querySelector('#username').classList.remove('hidden');
+    document.querySelector('#username').addEventListener('input', function () {
+        localStorage.setItem('username', this.value.trim());
+    });
+} else {
+    document.querySelector('.greet').textContent = `Привет, ${username}!`;
 }
-
-
-const goodClicklHandler = (item) => {
-	item.addEventListener("click", function () {
-		cart.innerHTML = ""
-		const itemIndex = this.getAttribute("data-good-id")
-		if (!goodsInCart.some((good) => good.name == goods[itemIndex].name)) {
-			goodsInCart.push(goods[itemIndex]);
-		}
-		else {
-			alert('Данный товар уже пристуствует в вашей корзине')
-		}
-		localStorage.setItem("goods", JSON.stringify(goodsInCart))
-		addItemFromStorage()
-	})
-}
-
-
-goods.forEach((good, index) => {
-	const item = document.createElement("div");
-	item.dataset.goodId = index;
-	item.innerText = `Марка ${good.name}, цена${good.price}`;
-	store.append(item);
-
-
-	goodClicklHandler(item);
-});
-
-
-addItemFromStorage()
-
-const clearStorage = document.querySelector(".clear-storage");
-
-clearStorage.addEventListener("click", () => {
-	localStorage.clear();
-	location.reload()
-});
-
-
-
-
-
-
-
-
-
 
 
 
